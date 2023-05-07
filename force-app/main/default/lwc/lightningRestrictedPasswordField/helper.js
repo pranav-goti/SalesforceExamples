a429781cbb1e080e9f019d4a55343935d2457a30
pranav-goti <43970894+pranav-goti@users.noreply.github.com>
2023-05-07 12:02:53 +0530

    helper

---
+++ force-app/main/default/lwc/lightningRestrictedPasswordField/helper.js
@@ -0,0 +1,40 @@
+const validationResult = {
+    isUpper : {
+        className : 'align-items',
+        iconName : 'utility:close',
+        iconSize : 'xx-small',
+        alternativeText : '',
+        title : '',
+        message : 'Upper Case',
+        isValid : false
+    },
+    isSpecial : {
+        className : 'align-items',
+        iconName : 'utility:close',
+        iconSize : 'xx-small',
+        alternativeText : '',
+        title : '',
+        message : 'Special Character',
+        isValid : false
+    },
+    isNumber : {
+        className : 'align-items',
+        iconName : 'utility:close',
+        iconSize : 'xx-small',
+        alternativeText : '',
+        title : '',
+        message : 'Numbers',
+        isValid : false
+    },
+    isLength : {
+        className : 'align-items',
+        iconName : 'utility:close',
+        iconSize : 'xx-small',
+        alternativeText : '',
+        title : '',
+        message : 'More than 8 Characters',
+        isValid : false
+    }
+}
+
+export { validationResult }
